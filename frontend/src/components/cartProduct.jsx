956703c6e54d2ead73e1ec5a065d222bbff93db9
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-02-21 11:19:46 +0530

    milestone20

--- frontend/src/components/cartProduct.jsx
+++ frontend/src/components/cartProduct.jsx
@@ -33,7 +33,7 @@ export default function CartProduct({ _id, name, images, quantity, price }) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                email: 'gowrishvarma@gmail.com',
+                email: 'Pranav@gmail.com',
                 productId: _id,
                 quantity,
             }),
